f61bef38ce08201dce92ec04fd8edaee7d8a7106
jayendra <jayendra@infocusp.in>
2015-08-06 18:01:07 +0530

    added space between pronounciation

--- random_words.py
+++ random_words.py
@@ -28,7 +28,7 @@ for w in words:
 		p2 = p2[0].getText().strip()
 
 	print '**********************************'
-	print w + ' (' + p1 + p2 +')'
+	print w + ' (' + p1 + ' ' + p2 +')'
 	
 	for r in results1:
 		print '\t' + r.getText().strip()
