c29547e4bf16ffe337eefcd0e6c463d1365b2a94
jayendra <jayendra@infocusp.in>
2015-08-06 17:10:25 +0530

    added gujrati pronounciation

--- random_words.py
+++ random_words.py
@@ -3,23 +3,30 @@ import numpy as np
 from bs4 import BeautifulSoup
 import requests
 
-df = pd.read_csv('/Users/Nisarg/Desktop/lst', header=None)
+df = pd.read_csv('lst', header=None)
 v1 = df[0].as_matrix()
 
 words = v1[np.random.randint(0, high=len(v1), size=(10,))]
 
 for w in words:
-	r = requests.get('http://dictionary.reference.com/browse/%s?s=t'%(w))
-	soup = BeautifulSoup(r.text, 'html.parser')
-	results = soup.findAll('div', attrs={'class': 'def-content'})
+	r1 = requests.get('http://dictionary.reference.com/browse/%s?s=t'%(w))
+	soup1 = BeautifulSoup(r1.text, 'html.parser')
+	results1 = soup1.findAll('div', attrs={'class': 'def-content'})
 	
-	print '*********************'
-	print w
-	for r in results:
+	r2 = requests.get('http://www.gujaratilexicon.com/dictionary/english-to-gujarati/%s*/'%(w))
+	soup2 = BeautifulSoup(r2.text, 'html.parser')
+	results2 = soup2.findAll('div', attrs={'class': 'meaning'})
+	p2 = soup2.findAll('div', attrs={'class': 'sw-3'})
+	if len(p2) == 0:
+		p2 = ''
+	else:
+		p2 = p2[0].getText().strip()
+
+	print '**********************************'
+	print w + ' (' + p2 +')'
+	
+	for r in results1:
 		print '\t' + r.getText().strip()
 		
-	r = requests.get('http://www.gujaratilexicon.com/dictionary/english-to-gujarati/%s*/'%(w))
-	soup = BeautifulSoup(r.text, 'html.parser')
-	results = soup.findAll('div', attrs={'class': 'meaning'})
-	for r in results:
+	for r in results2:
 		print '\t' + r.getText().strip()
